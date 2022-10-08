6d6336f1ededc1eff36f67d88b045f4a46373aa9
PhuRuji <59276795+phubet16132@users.noreply.github.com>
2022-10-08 12:06:48 +0700

    timedate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import './css/Home.css'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+      Home
+      </div>
+      <div className="enrollTime">
+        <input type="datetime-local" className='dtl'/>
+        <span>ถึง</span>
+        <input type="datetime-local" className='dtl'/>
+      </div>
+
+    </div>
   )
 }
 
