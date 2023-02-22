44c3af9f3b5cc4044ba0ae90ca4b39f9135b5cd9
PhuRuji <59276795+phubet16132@users.noreply.github.com>
2023-02-22 14:38:52 +0700

    show bracket

--- src/components/MatchContrianer.jsx
+++ src/components/MatchContrianer.jsx
@@ -11,72 +11,73 @@ const MatchContrianer = (props) => {
       return () => navigate(generatePath(ClientRounteKey.getTeamEach, {id}))
     }
 
-    const round1UpperMatches = props.tourMatch
-      .filter((match) => match.round === "1" && match.bracket ==="UPPER")
-      .sort((a,b) => a.pair - b.pair)
-      .map((match) =>
-        <div className="matchList">
-            <div className="pair">Pair: {match.pair}</div>
+    function matchShow(round, bracket){
+       return props.tourMatch
+        .filter((match) => match.round === round && match.bracket === bracket)
+        .sort((a,b) => a.pair - b.pair)
+        .map((match) =>
+          <div className="matchList">
+              <div className="pair">Pair: {match.pair}</div>
 
-            <div className="matchDetail">
-                <div className="teamAndScore">
-                  <div className="teamName" onClick={onTeamClick(match.homeTeamData.id)}>{match.homeTeamData.teamName}</div> 
-                  <div className="score"> 1 </div> 
-                </div>
-                <div className="vs">vs</div>
-                <div className="teamAndScore">
-                  <div className="score"> 2 </div>  
-                  <div className="teamName" onClick={onTeamClick(match.awayTeamData.id)}>{match.awayTeamData.teamName}</div>
-                </div>
-             </div>
-            
-            <div className="playTime"> Date: {(match.date).slice(0,10)}  Time: {(match.date).slice(11,16)}</div>
-         
-        </div>
-    );
+              <div className="matchDetail">
+                  <div className="teamAndScore">
+                    <div className="teamName" onClick={onTeamClick(match.homeTeamData.id)}>{match.homeTeamData.teamName}</div> 
+                    <div className="score"> {match.matchResult === "" ? "" : match.matchResult.teamHomeScore} </div> 
+                  </div>
+                  <div className="vs">vs</div>
+                  <div className="teamAndScore">
+                    <div className="score"> {match.matchResult === "" ? "" : match.matchResult.teamAwayScore} </div>  
+                    <div className="teamName" onClick={onTeamClick(match.awayTeamData.id)}>{match.awayTeamData.teamName}</div>
+                  </div>
+              </div>
+              
+              <div className="playTime"> Date: {(match.date).slice(0,10)}  Time: {(match.date).slice(11,16)}</div>
+          
+          </div>)
+  };
 
   return (
     <div className="matchWrape">
         <div className="Bracket">Upper Bracket</div>
         <div className="round">Round 1</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("1", "UPPER")}
         </div>
 
         <div className="round">Round 2</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("2", "UPPER")}
         </div>
 
         <div className="round">Upper Final</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("2", "UPPER")}
         </div>
 
         <div className="Bracket">Lower Bracket</div>
         <div className="round">Round 1</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("1", "LOWWER")}
         </div>
 
         <div className="round">Round 2</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("2", "LOWWER")}
         </div>
 
         <div className="round">Round 3</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("3", "LOWWER")}
         </div>
 
         <div className="round">Lower Final</div>
         <div className="matchRow">
-          {round1UpperMatches}
+          {matchShow("4", "LOWWER")}
         </div>
 
         <div className="Bracket">Grand Final</div>
         <div className="matchRow">
-          {round1UpperMatches}
+         {matchShow("4", "UPPER")}
         </div>
 
     </div>
